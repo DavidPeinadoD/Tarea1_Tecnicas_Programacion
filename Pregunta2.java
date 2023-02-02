fddbdce48dc05a009f135e091fbed982ae90c86f
DavidPeinadoD <113924648+DavidPeinadoD@users.noreply.github.com>
2023-02-02 18:48:23 +0000

    segunda version de mi codigo

--- Pregunta2.java
+++ Pregunta2.java
@@ -1,3 +1,32 @@
 // ¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
 
-//codigo de java para la pregunta 
+//codigo de java para el algoritmo de ordenamiento de burbuja en forma recursiva con programacion orientada a objetos
+package com.thealgorithms.sorts; 
+
+
+
+public class BubbleSort {
+    public int[] sort(int[] unsorted) {
+        int[] sorted = new int[unsorted.length];
+        for (int i = 0; i < unsorted.length; i++) {
+            sorted[i] = unsorted[i];
+        }
+        return sort(sorted, sorted.length);
+    }
+
+    private int[] sort(int[] unsorted, int n) {
+        if (n == 1) {
+            return unsorted;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (unsorted[i] > unsorted[i + 1]) {
+                int temp = unsorted[i];
+                unsorted[i] = unsorted[i + 1];
+                unsorted[i + 1] = temp;
+            }
+        }
+        return sort(unsorted, n - 1);
+    }
+}
+
+
