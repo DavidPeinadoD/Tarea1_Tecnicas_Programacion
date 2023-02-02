9ccdada63828025bf8451c433941548f754c1670
DavidPeinadoD <113924648+DavidPeinadoD@users.noreply.github.com>
2023-02-02 18:44:37 +0000

    hemos creado las preguntas

---
+++ Pregunta1.java
@@ -0,0 +1,8 @@
+// ¿Qué algoritmos usas en tu vida cotidiana? 
+
+//Path: Pregunta2.java
+
+//¿Piensas que podrías escribir un programa para hacerlos más eficientes?
+
+//Path: Pregunta3.java
+
---
+++ Pregunta2.java
@@ -0,0 +1,3 @@
+// ¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
+//codigo de java para la pregunta 
